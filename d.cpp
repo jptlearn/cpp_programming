866cd5d42b6668faac1340cc802e698a147d0dae
skcvlearn404 <skcvlearn404@gmail.com>
2025-02-01 16:50:46 +0545

    array in cpp

---
+++ d.cpp
@@ -0,0 +1,20 @@
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//   int arr[3][5]; // 2-D Array
+//   arr[1][3] = 78;
+//   cout << arr[1][2];
+//   return 0;
+// }
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  string s = "Striver";
+  cout << s[0];
+  cout << s[1];
+  cout << s.size();
+  return 0;
+}
