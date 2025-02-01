59fe46e1ec2318584059174d60c2eb9ecd881479
skcvlearn404 <skcvlearn404@gmail.com>
2025-02-01 16:23:29 +0545

    learn data types

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main()
+{
+  // int, long, long long, float, double
+  // string and getline
+  // char
+  char ch;
+  cout << "enter the character: " << endl;
+
+  cin >> ch;
+  cout << "The entered charactered is " << ch << endl;
+
+  return 0;
+}
