c84c8809ff57a4a8ba0f239bf58235569ffcaf55
skcvlearn404 <skcvlearn404@gmail.com>
2025-02-01 17:00:17 +0545

    basic of for and while loop in cpp

---
+++ e.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  string s = "Striver";
+  for (int i = 0; i <= 10; i++) {
+    cout << "Birendra" << endl;
+  }
+  int j = 1;
+  while (j <= 5)
+  {
+    cout << "Birendra" << j << endl;
+    j++;
+  }
+  int k = 1;
+  do
+  {
+    cout << "print me at least one time " << endl;
+    k++;
+  } while (k <= 1);
+  return 0;
+}
